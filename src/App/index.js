62ff21cd2f31ea33bb348e60a863dc95685de239
collpa <72576026+collpa@users.noreply.github.com>
2020-12-21 18:36:54 +0100

    Change Components' organization

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+import Header from "../Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
