6d298d230bcdcc261fcb38afdc99b416c2d07637
collpa <72576026+collpa@users.noreply.github.com>
2020-12-21 18:42:14 +0100

    Imported Player component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles.scss";
 import Header from "../Header";
+import Player from "../Player";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <div>
+          <Player />
+        </div>
       </div>
     );
   }
