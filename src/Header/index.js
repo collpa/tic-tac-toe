245bb2ed5ddbea520063e43ef7d036e7a19eaa87
collpa <72576026+collpa@users.noreply.github.com>
2020-12-21 18:35:05 +0100

    Create Header

---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+class Header extends React.Component {
+  render() {
+    return <h1>LET'S THE GAME BEGIN!</h1>;
+  }
+}
+
+export default Header;
