aec1ed2321c00168cec337631acfd67c6b9db24d
collpa <72576026+collpa@users.noreply.github.com>
2021-01-08 11:03:59 +0100

    Added logic (game winning rules) to the board

--- src/Board/index.js
+++ src/Board/index.js
@@ -22,10 +22,80 @@ class Board extends React.Component {
         newBoardCells[positionOfItem] = 2;
       }
 
-      this.setState({
-        boardCells: newBoardCells,
-        turn: currentTurn + 1,
-      });
+      this.setState(
+        {
+          boardCells: newBoardCells,
+          turn: currentTurn + 1,
+        },
+        //the following "callback" is called when the setState has finished changing the state
+        () => this.checkWinner(newBoardCells)
+      );
+    }
+  }
+
+  checkWinner(boardCells) {
+    if (
+      boardCells[0] !== 0 &&
+      boardCells[0] === boardCells[1] &&
+      boardCells[1] === boardCells[2]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[3] !== 0 &&
+      boardCells[3] === boardCells[4] &&
+      boardCells[4] === boardCells[5]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[6] !== 0 &&
+      boardCells[6] === boardCells[7] &&
+      boardCells[7] === boardCells[8]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[0] !== 0 &&
+      boardCells[0] === boardCells[3] &&
+      boardCells[3] === boardCells[6]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[1] !== 0 &&
+      boardCells[1] === boardCells[4] &&
+      boardCells[4] === boardCells[7]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[2] !== 0 &&
+      boardCells[2] === boardCells[5] &&
+      boardCells[5] === boardCells[8]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[0] !== 0 &&
+      boardCells[0] === boardCells[4] &&
+      boardCells[4] === boardCells[8]
+    ) {
+      return true;
+    }
+
+    if (
+      boardCells[2] !== 0 &&
+      boardCells[2] === boardCells[4] &&
+      boardCells[4] === boardCells[6]
+    ) {
+      return true;
     }
   }
 
