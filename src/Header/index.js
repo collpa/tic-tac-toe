0e208da0b2add3a104a0f1dd257fa276d9d64a21
collpa <72576026+collpa@users.noreply.github.com>
2021-01-08 20:14:50 +0100

    Added posibility to reset the game once it ends

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,14 +2,37 @@ import React from "react";
 import "./styles.scss";
 
 class Header extends React.Component {
-  render() {
+  renderRestartGame() {
+    if (this.props.endGame) {
+      return <h3>Press any keys to reset the game!</h3>;
+    }
+  }
+
+  renderTitle() {
     if (this.props.haveWinner) {
-      return <h1>{`${this.props.playerName} WIN!`}</h1>;
+      return (
+        <>
+          <h1>{`${this.props.playerName} WIN!`}</h1>
+        </>
+      );
     } else if (this.props.endGame) {
-      return <h1>{"LOOKS LIKE IT'S DRAW"}</h1>;
+      return (
+        <>
+          <h1>{"LOOKS LIKE IT'S DRAW"}</h1>
+        </>
+      );
     } else {
       return <h1>{"LET THE GAME BEGIN!"}</h1>;
     }
+  }
+
+  render() {
+    return (
+      <div className="header-container">
+        {this.renderTitle()}
+        {this.renderRestartGame()}
+      </div>
+    );
   }
 }
 
