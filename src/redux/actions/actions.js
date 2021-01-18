7b67a03b6fcd7b33adfcedd00ad0e924a2af1bc8
collpa <72576026+collpa@users.noreply.github.com>
2021-01-18 18:11:15 +0100

    Created actions

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,24 @@
+export const UPDATE_TURN = "UPDATE_TURN";
+export const HAVE_WINNER = "HAVE_WINNER";
+export const END_GAME = "END_GAME";
+export const RESET_GAME = "RESET_GAME";
+
+export const updateTurn = {
+  type: UPDATE_TURN,
+  payload: null,
+};
+
+export const haveWinner = {
+  type: HAVE_WINNER,
+  payload: null,
+};
+
+export const endGame = {
+  type: END_GAME,
+  payload: null,
+};
+
+export const resetGame = {
+  type: RESET_GAME,
+  payload: null,
+};
